813ce50c25250138baec36901f5a87a8b3dcf1cb
internetcharles <charlesodellsmith@gmail.com>
2020-05-22 15:59:59 -0700

    added paddle logic to track with mouse cursor

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector3 paddlePos = new Vector3(mouseX, 0.47f, 1);
+        transform.position = paddlePos;
+
+    }
+}
